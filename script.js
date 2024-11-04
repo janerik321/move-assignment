ec8a14c8e1ffbe92284cc86200b2fd8d19701ae7
Jan Erik <janerik.kodehode@gmail.com>
2024-11-04 10:58:39 +0100

    Final version?

--- script.js
+++ script.js
@@ -1,54 +1,51 @@
 const ball = document.querySelector("#ball");
 const obstacle = document.querySelector("#obstacle");
-const body = document.querySelector("body");
-
-fieldX = 700;
-fieldY = 650;
-body.style.width = `${fieldX}px`;
-body.style.height = `${fieldY}px`;
-
-ball.style.zIndex = "1";
-obstacle.style.width = "100px";
-obstacle.style.height = "100px";
-obstacle.style.backgroundColor = "yellow";
-obstacle.style.position = "absolute";
 
 let ballX = 0;
 let ballY = 0;
+ball.style.zIndex = "1";
 
+// Obstacle values.
+const obstacleWidth = 100;
+const obstacleHeight = 100;
+obstacle.style.width = `${obstacleWidth}px`;
+obstacle.style.height = `${obstacleHeight}px`;
+obstacle.style.backgroundColor = "yellow";
+obstacle.style.position = "absolute";
+
+// Obstacle position.
 const obstacleX = 300;
 const obstacleY = 300;
 obstacle.style.left = `${obstacleX}px`;
 obstacle.style.top = `${obstacleY}px`;
 
+// Check for arrow keypress. Change X & Y positions accordingly.
 window.addEventListener("keydown", (e) => {
   if (e.key === "ArrowUp" && ballY > 5) {
     ballY -= 5;
-  } else if (e.key === "ArrowDown" && ballY < fieldY - 90) {
+  } else if (e.key === "ArrowDown" && ballY < innerHeight - 110) {
     ballY += 5;
   } else if (e.key === "ArrowLeft" && ballX > 5) {
     ballX -= 5;
-  } else if (e.key === "ArrowRight" && ballX < fieldX - 90) {
+  } else if (e.key === "ArrowRight" && ballX < innerWidth - 110) {
     ballX += 5;
   }
-  // else if (e.key === " ") {
-  //   ball.style.transition = "1s";
-  //   ball.style.width = "120px";
-  //   ball.style.height = "120px";
-  // }
 
-  // Collision detection
+  // Collision detection.
   if (
     ballX <= obstacleX - 100 ||
-    ballX >= obstacleX + 100 ||
+    ballX >= obstacleX + obstacleWidth ||
     ballY <= obstacleY - 100 ||
-    ballY >= obstacleY + 100
+    ballY >= obstacleY + obstacleHeight
   ) {
+    // Move ball according to arrow direction.
     ball.style.transition = "50ms";
     ball.style.top = `${ballY}px`;
     ball.style.left = `${ballX}px`;
     ball.style.backgroundPosition = `${ballX}px ${ballY}px`;
-  } else if (e.key === "ArrowUp") {
+  }
+  // If collision occurs, set X & Y to previous values.
+  else if (e.key === "ArrowUp") {
     ballY += 5;
   } else if (e.key === "ArrowDown") {
     ballY -= 5;
@@ -59,11 +56,21 @@ window.addEventListener("keydown", (e) => {
   }
 });
 
+// Mouse click.
 document.addEventListener("click", (e) => {
-  ballY = e.pageY - 50;
-  ballX = e.pageX - 50;
-  ball.style.top = `${ballY}px`;
-  ball.style.left = `${ballX}px`;
-  ball.style.transition = "1s";
-  ball.style.backgroundPosition = `${ballX}px ${ballY}px`;
+  // Collision detection.
+  if (
+    e.pageX <= obstacleX - 50 ||
+    e.pageX >= obstacleX + obstacleWidth + 50 ||
+    e.pageY <= obstacleY - 50 ||
+    e.pageY >= obstacleY + obstacleHeight + 50
+  ) {
+    // Move ball to clicked area.
+    ballY = e.pageY - 50;
+    ballX = e.pageX - 50;
+    ball.style.top = `${ballY}px`;
+    ball.style.left = `${ballX}px`;
+    ball.style.transition = "1s";
+    ball.style.backgroundPosition = `${ballX}px ${ballY}px`;
+  }
 });
